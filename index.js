e25f90d30f27ae4b8b367773ee88cac55d1f8794
ArtRosh <artem.rosh17@gmail.com>
2025-02-10 15:59:05 -0800

    flow-lab

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if (distance <= 400) {
+    return 'This one is on me!';
+  }
+  else if (distance <=2000) {
+    return 'That will be twenty bucks.';
+  }
+  else if (distance <=2500) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  else {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+    return city === 'NYC'
+      ? 'Ok, sounds good.'
+      : city === 'Pittsburgh'
+        ? 'No go.'
+        :undefined;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    case 'thanks for everything':
+      return 'Bye.';
+    default :
+      return 'Have a nice day!';
+  }
 }
